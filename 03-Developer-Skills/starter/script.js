abc33abb3c8bc18f7857df880f6669f7fafb892b
dpawlov <dimitarpawlovvv@gmail.com>
2021-10-16 20:22:13 +0300

    commit

---
+++ 03-Developer-Skills/starter/script.js
@@ -0,0 +1,14 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+let data = [17, 21, 23];
+
+function printForecast(arr) {
+  let str = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}C in ${i + 1} `;
+  }
+
+  console.log(`${element}C in ${i} day`);
+}
